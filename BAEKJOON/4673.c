6077be0fea07a5c9f0b772cde19a0c953a59ce74
PrimWILL <jiyoon0043@ajou.ac.kr>
2020-09-04 01:39:34 +0900

    2020.09.04

--- BAEKJOON/4673.c
+++ BAEKJOON/4673.c
@@ -1,31 +1,23 @@
 #include <stdio.h>
 
-void self_num(int arr[], int num) {
-	if (num < 10) {
-		num = num + num;
-		arr[num - 1] = 1;
+void self_num(int* arr, int num) {
+	int n = num;
+	while (1) {
+		if (num == 0) break;
+		n += num%10;
+		num /= 10;
 	}
-	else if (num < 100) {
-		num = num + num/10 + num%10;
-		arr[num - 1] = 1;
-	}
-	else if (num < 1000) {
-		num = num + num/100 + (num/10)%10 + num%10;
-		arr[num - 1] = 1;
-	}
-	else if (num < 10000) {
-		num = num + num/1000 + (num/100)%10 + (num/10)%10 + num%10;
-		if (num <= 10000) arr[num - 1] = 1;
-	}
+	if (n <= 10000)
+		arr[n - 1]++;
 }
 
 int main(void) {
-	int number[10001] = { 0 };
+	int number[10000] = { 0 };
 	for (int i = 1; i <= 10000; i++) {
 		self_num(number, i);
 	}
 	for(int i = 0; i < 10000; i++) {
-		if (!number[i]) {
+		if (!*(number + i)) {
 			printf("%d\n", i + 1);
 		}
 	}
